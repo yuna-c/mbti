59878fe953001027d19d69a9c05d8948216b88bc
yuna <yuna744@gmail.com>
2024-09-10 20:20:33 +0900

    📝 add : 회원가입, 로그인, 로그아웃, 닉네임 수정 query 분리 완료

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,30 +1,14 @@
 import { useState } from 'react';
-import { updateProfile } from '../../api/auth';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import useAuthStore from '../../store/useAuthStore';
+import { useProfile } from '../../hook/useAuth';
 import Button from '../common/ui/Button';
 import Input from '../common/ui/Input';
 import Article from '../common/ui/Article';
 
 export default function Profile() {
-  const queryClient = useQueryClient();
   const [nickname, setNickname] = useState('');
-  const token = useAuthStore((state) => state.accessToken);
-  // const [updatedNickname, setUpdatedNickname] = useState('');
-  const setNicknameInStore = useAuthStore((state) => state.setNickname);
+  const { mutate } = useProfile();
 
-  const { mutate } = useMutation({
-    mutationFn: (newNickname) => updateProfile(token, { nickname: newNickname }),
-    onSuccess: () => {
-      setNicknameInStore(nickname);
-      queryClient.invalidateQueries(); // 캐시된 쿼리 무효화
-    },
-    onError: (error) => {
-      console.error('닉네임 변경 실패:', error);
-    }
-  });
-
-  const handleNicknameChange = async () => {
+  const handleNicknameChange = () => {
     mutate(nickname);
   };
 
--- src/hook/useAuth.js
+++ src/hook/useAuth.js
@@ -1,6 +1,45 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import useAuthStore from '../store/useAuthStore';
-import { login, register } from '../api/auth';
+import { login, register, updateProfile } from '../api/auth';
+
+// 회원가입
+export const useSignUp = () => {
+  const queryClient = useQueryClient();
+  const setAuth = useAuthStore((state) => state.setAuth);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: (data) => {
+      setAuth(data.accessToken, data.nickname, data.userId);
+      queryClient.invalidateQueries();
+    },
+    onError: (error) => {
+      console.error('회원가입 실패:', error.response?.data || error.message);
+    }
+  });
+
+  return mutation;
+};
+
+// 로그인
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  const setAuth = useAuthStore((state) => state.setAuth);
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      const { accessToken, nickname, userId } = data;
+      setAuth(accessToken, nickname, userId);
+      queryClient.invalidateQueries();
+    },
+    onError: (error) => {
+      console.error('로그인 실패:', error.response?.data || error.message);
+    }
+  });
+
+  return mutation;
+};
 
 // 로그아웃
 export const useLogout = () => {
@@ -13,35 +52,22 @@ export const useLogout = () => {
   return { accessToken, nickname, clearAuth };
 };
 
-// 회원가입
-export const useSignUp = () => {
-  const setAuth = useAuthStore((state) => state.setAuth);
+// 닉네임 수정
+export const useProfile = () => {
+  const queryClient = useQueryClient();
+  const { token, setNicknameInStore } = useAuthStore((state) => ({
+    token: state.accessToken,
+    setNicknameInStore: state.setNickname
+  }));
 
   const mutation = useMutation({
-    mutationFn: register,
-    onSuccess: (data) => {
-      setAuth(data.accessToken, data.nickname, data.userId);
+    mutationFn: (newNickname) => updateProfile(token, { nickname: newNickname }),
+    onSuccess: (data, newNickname) => {
+      setNicknameInStore(newNickname); // newNickname을 store에 저장
+      queryClient.invalidateQueries(); // 캐시된 쿼리 무효화
     },
     onError: (error) => {
-      console.error('회원가입 실패:', error.response?.data || error.message);
-    }
-  });
-
-  return mutation;
-};
-
-// 로그인
-export const useLogin = () => {
-  const setAuth = useAuthStore((state) => state.setAuth);
-
-  const mutation = useMutation({
-    mutationFn: login,
-    onSuccess: (data) => {
-      const { accessToken, nickname, userId } = data;
-      setAuth(accessToken, nickname, userId);
-    },
-    onError: (error) => {
-      console.error('로그인 실패:', error.response?.data || error.message);
+      console.error('닉네임 변경 실패:', error);
     }
   });
 
