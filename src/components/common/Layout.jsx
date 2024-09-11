fbe79b4d2f14b0a27c78efa6537bfe9a113faed3
yuna <yuna744@gmail.com>
2024-09-11 17:06:03 +0900

    📝 add : style 전체 완료

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -7,12 +7,12 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="relative z-10 p-3 opacity-100 md:px-6 md:py-4 Layout">
+      <main className="relative z-10 p-3 md:px-6 md:py-4 Layout">
         <div className="fixed pointer-events-none w-700px -z-10 -top-20 -right-12 opacity-85 Bg">
           <img src={bgImage} alt="bgImage" className="w-auto h-auto" />
         </div>
 
-        <section className="min-h-[calc(100vh-11rem)] z-10 top-0 ">
+        <section className="min-h-[calc(100vh-11rem)] z-10 top-0 opacity-90 ">
           <Outlet />
         </section>
       </main>
