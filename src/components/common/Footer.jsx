92e058bb2e324416057e5042f8fc3a4ae7e422b0
yuna <yuna744@gmail.com>
2024-09-11 17:11:35 +0900

    📝 add : 반응형 수정사항 없이 전부 완료

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="relative z-50 p-3 border-t opacity-100 md:px-6 md:py-4 border-stone-950 md:opacity-80 Footer">
-      <div className="w-full space-y-4 text-sm bg-white md:text-lg md:flex md:items-center md:justify-between md:w-auto md:p-0 md:bg-transparent">
+      <div className="w-full space-y-4 text-sm md:text-lg md:flex md:items-center md:justify-between md:w-auto md:p-0 md:bg-transparent">
         20240904-20240911 / fonts by googleFont / Icons by fontAwesome / Image by pixabay(Bucarama-TLM) / Designed by
         yuna
       </div>
