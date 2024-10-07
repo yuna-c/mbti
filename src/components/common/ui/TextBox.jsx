e6b08e8ed28a922876091d4c5248312f581016aa
yuna <yuna744@gmail.com>
2024-10-07 19:27:23 +0900

    📝 add: data.id 부분 index 삭제

--- src/components/common/ui/TextBox.jsx
+++ src/components/common/ui/TextBox.jsx
@@ -2,7 +2,7 @@ export default function TextBox({ data }) {
   return (
     <div
       className="flex flex-col w-full gap-3 p-4 border border-black rounded-lg shadow-md md:w-1/3 hover:shadow-lg TextBox"
-      key={data.id || index}
+      key={data.id}
     >
       <h2>{data.title}</h2>
       <p>{data.content}</p>
