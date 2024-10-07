59a22d668cbceecbf40e29e1e857986efaae2a8a
yuna <yuna744@gmail.com>
2024-10-07 19:32:04 +0900

    🐛fix: useAuthStore  persist 추가

--- src/core/stores/useAuthStore.js
+++ src/core/stores/useAuthStore.js
@@ -1,32 +1,30 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useAuthStore = create((set) => ({
-  // 초기값을 localStorage에서 가져옴
-  accessToken: localStorage.getItem('accessToken') || null,
-  nickname: localStorage.getItem('nickname') || null,
-  userId: localStorage.getItem('userId') || null,
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      accessToken: null,
+      nickname: null,
+      userId: null,
 
-  // 사용자 정보를 저장하는 함수
-  setAuth: (token, nickname, userId) => {
-    set({ accessToken: token, nickname: nickname, userId: userId });
-    localStorage.setItem('accessToken', token);
-    localStorage.setItem('nickname', nickname);
-    localStorage.setItem('userId', userId);
-  },
+      setAuth: (token, nickname, userId) => {
+        set({ accessToken: token, nickname: nickname, userId: userId });
+      },
 
-  // 닉네임 업데이트
-  setNickname: (nickname) => {
-    set({ nickname });
-    localStorage.setItem('nickname', nickname);
-  },
+      setNickname: (nickname) => {
+        set({ nickname });
+      },
 
-  // 로그아웃 함수 (모든 정보를 초기화)
-  clearAuth: () => {
-    set({ accessToken: null, nickname: null, userId: null });
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('nickname');
-    localStorage.removeItem('userId');
-  }
-}));
+      clearAuth: () => {
+        set({ accessToken: null, nickname: null, userId: null });
+      }
+    }),
+    {
+      name: 'auth-storage',
+      getStorage: () => localStorage
+    }
+  )
+);
 
 export default useAuthStore;
